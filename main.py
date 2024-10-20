3156f91d522141945267370f17464900ddb6a757
Minh Hải <117704352+abuhmhai@users.noreply.github.com>
2024-10-21 00:31:46 +0700

    photo

--- main.py
+++ main.py
@@ -4,8 +4,5 @@ from ultralytics import YOLO
 # Load a COCO-pretrained YOLO11n model
 model = YOLO("yolo11n.pt")
 
-# Train the model on the COCO8 example dataset for 100 epochs
-results = model.train(data="coco8.yaml", epochs=100, imgsz=640)
-
 # Run inference with the YOLO11n model on the 'bus.jpg' image
-results = model("./AQNCPkYWm6OLGBCJqCL2Zdr_OAnxz2REeYeP5DTjEGMLStQjzlIsJpaVKFUxBhvtw4sxL62We9vvnrIVW0Tb--m4.mp4", save=True, show=True)
+results = model("./JENNIE - Mantra (Official Music Video).mp4", save=True, show=True)
